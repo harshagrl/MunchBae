4ea08828f0c0761fe207e65042135e4f242e7f51
harshagrl <harshkr.agrl@gmail.com>
2026-06-04 20:45:44 +0530

    fix: update live location mark

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -6,7 +6,19 @@ import { FaSearch } from "react-icons/fa";
 import { BiCurrentLocation } from "react-icons/bi";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import { useDispatch, useSelector } from "react-redux";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
 import { setAddress, setLocation } from "../store/map.slice";
 import { addMyOrder, clearCart } from "../store/user.slice";
 import axios from "axios";
